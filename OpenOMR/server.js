06f6478a2b7ac45db6399ea7a955f626c4df387b
Mohamed Eltuhamy <tuhamy@gmail.com>
2014-03-09 04:29:04 +0000

    server works with image posting, sitting across there

--- OpenOMR/server.js
+++ OpenOMR/server.js
@@ -1,5 +1,12 @@
 var express = require("express");
+var fs = require("fs");
 var app = express();
+
+
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.multipart());
+   });
 
 var spawn = require('child_process').spawn;
 var nextFileCounter = 0;
@@ -7,6 +14,7 @@ var nextFileCounter = 0;
 var server = app.listen(8080, function() {
     console.log('Listening on port %d', server.address().port);
 });
+
 
 app.get('/recogniseMusic', function(req, res){
   var child = spawn('bash', ['translate.sh', nextFileCounter++, 'http://localhost:'+server.address().port]);
@@ -19,5 +27,17 @@ app.get('/ls', function(req, res) {
   var child = spawn('ls', ['-al']);
   child.stdout.pipe(res);
 })
+
+app.post('/picupload', function(req, res) {
+	console.log(JSON.stringify(req.files));
+	fs.readFile(req.files.music.path, function (err, data) {	  // ...
+	  var newPath = __dirname + "/uploads/"+nextFileCounter;
+	  fs.writeFile(newPath, data, function (err) {
+	    var child = spawn('bash', ['translate.sh', 'uploads/'+nextFileCounter, nextFileCounter++, 'http://localhost:'+server.address().port]);
+	    child.stdout.pipe(res);
+	  });
+	});
+});
+
 
 app.use(express.static(__dirname + '/'));
--- OpenOMR/translate.sh
+++ OpenOMR/translate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -classpath lib/joone-engine.jar:lib/jcommon-1.0.22.jar:.:lib/jfreechart-1.0.2.jar -Xmx256m openomr/openomr/CreateMidiForImage testImages/demo/scale.png $1 >/dev/null
-echo $2/$1.mid
+java -classpath lib/joone-engine.jar:lib/jcommon-1.0.22.jar:.:lib/jfreechart-1.0.2.jar -Xmx256m openomr/openomr/CreateMidiForImage $1 $2 >/dev/null
+echo $3/$2.mid
